718e91687da7f7b4ca1c8ebdc442f790cc719640
Parker Roth <parkeroth@gmail.com>
2011-09-08 15:07:09 -0500

    Started work on the Event class

--- www/test.php
+++ www/test.php
@@ -1,22 +1,5 @@
 <?php
-<<<<<<< HEAD
 
 phpinfo();
-=======
-require_once 'classes/Event.php';
-
-$event = new Event();
-//echo $event->type;
-$event->title = 'test';
-$event->event_date = '2011-11-11';
-$event->term = 'spring';
-$event->time = '1:00 AM';
-$event->type = 'general';
-$event->insert();
-echo $event->id;
-
-$event->delete();
-echo $event->id;
->>>>>>> database_interaction_update
 
 ?>
