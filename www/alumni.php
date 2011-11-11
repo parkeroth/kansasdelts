1649e95ed68c779928e203397a92998d3e80adba
Parker Roth <parker@Parker-Ubuntu.(none)>
2011-11-11 13:51:32 -0600

    added newsletter take 2

--- www/alumni.php
+++ www/alumni.php
@@ -8,6 +8,7 @@
 	  <p><img src="img/alumni" width="255" height="195" alt="alumni" align="right" style="margin:20px; margin-top:0px;" />Welcome Delta Tau Delta Alums! The Gamma Tau Chapter hosts a number of events per year, just for you. 
 	  	Getting in touch with the men that made our organization what it is today is always an important event. Come support 
 	  	the chapter by showing up to events and having a good time with us.</p>
+	  <p><a href="newsletters/newsletter-F11.pdf">Click here</a> to view the Fall 2011 alumni newsletter.</p>
 	  <p><a href="newsletters/newsletter-S11.pdf">Click here</a> to view the Spring 2011 alumni newsletter.</p>
 	  <p>&nbsp;</p>
 	  <p>&nbsp;</p>
