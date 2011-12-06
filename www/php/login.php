71906dab33beb91d1f865ac05d807e52469898d4
webmaster <webmaster@kansasdelts.org>
2011-12-06 16:25:35 -0600

    Fixed Popup position

--- www/php/login.php
+++ www/php/login.php
@@ -2,6 +2,6 @@
 $db_host='localhost';
 $db_database='delt';
 $db_username='root';
-$db_password='';
+$db_password='DTD1856GammaTau';
 $mysqli = mysqli_connect($db_host, $db_username, $db_password, $db_database);
 ?>
