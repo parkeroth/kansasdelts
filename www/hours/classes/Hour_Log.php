28b43c405045680250198c6b1a082dc9130499be
Parker Roth <parkeroth@gmail.com>
2012-02-14 17:32:18 -0600

    schema change

--- www/hours/classes/Hour_Log.php
+++ www/hours/classes/Hour_Log.php
@@ -2,20 +2,29 @@
 include_once $_SERVER['DOCUMENT_ROOT'].'/core/classes/DB.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/core/classes/Position.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/core/util.php';
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  * Description of Hour_Log
  *
  * @author Parker Roth
+ * 
+ * CREATE TABLE IF NOT EXISTS `hourLog` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `member_id` int(11) NOT NULL,
+  `term` set('fall','spring') NOT NULL,
+  `year` int(4) NOT NULL,
+  `hours` int(4) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `eventID` int(11) DEFAULT NULL,
+  `dateAdded` date NOT NULL,
+  `notes` varchar(100) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1711 ;
+ * 
  */
 class Hour_Log extends DB_Table {
 	
 	public $id = NULL;
-	public $username = NULL;				//Deprecated
 	public $member_id = NULL;
 	public $type = NULL;
 	public $term = NULL;
@@ -35,7 +44,6 @@ class Hour_Log extends DB_Table {
 		$this->table_mapper = array(
 			'id' => 'ID',
 			'member_id' => 'member_id',
-			'username' => 'username',		//Deprecated
 			'term' => 'term',
 			'year' => 'year',
 			'hours' => 'hours',
