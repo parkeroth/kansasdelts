b6a3a82db34549f1f5a9c98650532e7940c4834e
Nathan Disidore <ndisidore@gmail.com>
2012-02-26 20:27:22 -0600

    beginning stages of integration with Flickr API

--- www/photos/service.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	session_start();
-	include_once($_SERVER['DOCUMENT_ROOT']."/includes/headerFirst.php");
-	include_once($_SERVER['DOCUMENT_ROOT']."/includes/headerLast.php");
-?>
-
-<h1>Service Events</h1>
-
-<?php include_once($_SERVER['DOCUMENT_ROOT']."/includes/footer.php"); ?>
