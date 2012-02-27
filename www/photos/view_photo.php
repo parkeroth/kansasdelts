e674fda91a389a9fc7810ec4088289d8a0c2abb2
Nathan Disidore <ndisidore@gmail.com>
2012-02-26 20:28:50 -0600

    new files using Flickr API

---
+++ www/photos/view_photo.php
@@ -0,0 +1,33 @@
+<?php
+	session_start();
+	include_once($_SERVER['DOCUMENT_ROOT']."/includes/headerFirst.php");
+	include_once($_SERVER['DOCUMENT_ROOT']."/includes/headerLast.php");
+        include_once 'auth.php';
+
+        $photo_id = $_GET['photo_id'];            //desired photo id
+
+        //require_once("phpFlickr.php");
+        //$f = new phpFlickr($api_key);
+
+        //get photo info
+        $photo_info = $f->photos_getInfo($photo_id);
+        //get photo sizes
+        $photo_sizes = $f->photos_getSizes($photo_id);
+
+        foreach($photo_sizes['sizes'] as $photo_size) {
+            //we'll only grab the medium size
+            if($photo_size['label'] == "Medium") {
+                $photo_src = $photo_size['source'];
+            }
+        }
+
+?>
+<h2 class="photo_image_title"><?php echo $photo_info['title']; ?></h2>
+
+<div id="individual_photo">
+    <img src="<?php echo $photo_src; ?>" alt="<?php echo $photo_info['title']; ?>" />
+</div>
+
+<a href="<?php echo $photo_info['urls']['url']['photopage'] ?>" title="View this image on Flickr">[ View This Image on Flickr ]</a>
+
+<?php include_once($_SERVER['DOCUMENT_ROOT']."/includes/footer.php"); ?>
